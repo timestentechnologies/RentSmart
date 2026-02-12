0020794bc9a9d3f74ecd1b830b94800881ebb325
timestentechnologies <timestentechnologies@gmail.com>
2026-02-12 09:13:47 +0300

    dashboard

--- app/Controllers/PaymentMethodsController.php
+++ app/Controllers/PaymentMethodsController.php
@@ -28,7 +28,7 @@ class PaymentMethodsController
             $isAdmin = in_array($role, ['administrator', 'admin'], true);
 
             $paymentMethods = $isAdmin
-                ? $this->paymentMethod->getAllForScope('subscription')
+                ? $this->paymentMethod->getActiveForScope('subscription')
                 : $this->paymentMethod->getByUser($userId);
             
             // Properties for linking in the UI
