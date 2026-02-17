639b340ea6859a6a17f4db3293026170b4ed5b6c
timestentechnologies <timestentechnologies@gmail.com>
2026-02-17 16:04:05 +0300

    fix realtor leads corrupted modal markup

--- views/realtor/leads.php
+++ views/realtor/leads.php
@@ -352,7 +352,17 @@ ob_start();
       <div class="modal-header">
         <h5 class="modal-title">Confirm Delete</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
- div class="modal fade" id="realtorLeadAddListingModal" tabindex="-1" aria-hidden="true">
+      </div>
+      <div class="modal-body">Are you sure you want to delete this lead?</div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button type="button" id="confirmDeleteLeadBtn" class="btn btn-danger">Delete</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="realtorLeadAddListingModal" tabindex="-1" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <form id="realtorLeadAddListingForm" enctype="multipart/form-data">
@@ -385,22 +395,22 @@ ob_start();
               <input type="number" step="0.01" name="price" class="form-control" required>
             </div>
           </div>
-          <div clas ="mb-3">
-            <label  lass="form-label">Status</label>
-            <select name="status" class="form-select" requi ed>
-              <option value="active">Act ve</o <ion/div>
+          <div class="mb-3">
+            <label class="form-label">Status</label>
+            <select name="status" class="form-select" required>
+              <option value="active">Active</option>
               <option value="inactive">Inactive</option>
               <option value="sold">Sold</option>
               <option value="rented">Rented</option>
             </select>
           </div>
           <div class="mb-3">
-            <label class=" orm-label">Description</label>
+            <label class="form-label">Description</label>
             <textarea name="description" class="form-control" rows="3"></textarea>
           </div>
           <div class="mb-3">
             <label class="form-label">Images</label>
-            <inp t type="file"  ame="listing_images[]" class="form-control" accept="image/*" multiple>
+            <input type="file" name="listing_images[]" class="form-control" accept="image/*" multiple>
           </div>
           <div class="alert alert-danger d-none" id="realtorLeadAddListingError"></div>
         </div>
@@ -414,10 +424,6 @@ ob_start();
 </div>
 
 <script>
-fun   <div class="modal-body">Are you sure you want to delete this lead?</div>
-      <div class="modal-footer">
- 
-
 (function(){
   function upsertOption(selectEl, id, label){
     if(!selectEl) return;
@@ -514,12 +520,8 @@ fun   <div class="modal-body">Are you sure you want to delete this lead?</div>
       if(addListingSubmit) addListingSubmit.disabled = false;
     }
   });
-})();       <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-        <button type="button" id="confirmDeleteLeadBtn" class="btn btn-danger">Delete</button>
-      </div>
-    </div>
-  </div>
-</div>
+})();
+</script>
 
 <script>
 function getAllStageKeys(){
